110298fafd7f28b2e15f1756605d68f54ed29105
Ingyun Son <ingyunson@gmail.com>
2018-11-14 23:34:48 +0900

    Update: update import news info and send message with news info

--- chatbot_assistant.py
+++ chatbot_assistant.py
@@ -1,7 +1,12 @@
 import telegram
+import yahoo_news_jp
 
 my_token = '711334098:AAHhNP3Hondc76pJcbOlNXPtJ2f0HlYYVBk'
 chatbot = telegram.Bot(token=my_token)
 chat_id = chatbot.getUpdates()[-1].message.chat.id
+text1 = yahoo_news_jp.headline_title
+text2 = yahoo_news_jp.headline_url
+print(text1)
+print(text2)
 
-chatbot.sendMessage(chat_id=chat_id, text='Type your message')
+chatbot.sendMessage(chat_id=chat_id, text=text2)
